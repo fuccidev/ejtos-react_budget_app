b6e6f3afb1cd204b239ccf76b836c102a75e5e87
Alexander Fucci-Wilson <alexander.fucciwilson@csuglobal.edu>
2024-01-04 20:51:15 -0500

    completed final budget project

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,25 +3,26 @@ import ExpenseItem from './ExpenseItem';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseList = () => {
-    const { expenses } = useContext(AppContext);
+    const { expenses, currency } = useContext(AppContext);
 
     return (
         <table className='table'>
-              <thead className="thead-light">
-            <tr>
-              <th scope="col">Department</th>
-              <th scope="col">Allocated Budget</th>
-              <th scope="col">Increase by 10</th>
-              <th scope="col">Delete</th>
-            </tr>
-          </thead>
+            <thead className="thead-light">
+                <tr>
+                    <th scope="col">Department</th>
+                    <th scope="col">Allocated Budget</th>
+                    <th scope="col">Increase by 10</th>
+                    <th scope="col">Decrease by 10</th>
+                    <th scope="col">Delete</th>
+                </tr>
+            </thead>
             <tbody>
-            {expenses.map((expense) => (
-                <ExpenseItem id={expense.id} key={expense.id} name={expense.name} cost={expense.cost} />
-            ))}
+                {expenses.map((expense) => (
+                    <ExpenseItem id={expense.id} key={expense.id} name={expense.name} cost= {currency + expense.cost}/>
+                ))}
             </tbody>
         </table>
     );
 };
 
-export default ExpenseList;
+export default ExpenseList; 
